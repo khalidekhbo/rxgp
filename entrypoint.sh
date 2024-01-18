a5e6a2a643e2bc6420607ba27d52547249afa448
khalidekhbo <112927910+khalidekhbo@users.noreply.github.com>
2024-01-19 00:02:59 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create the directory if it doesn't exist
+mkdir -p /root/.config/rclone/
+
 # Fetch the Rclone config from the environment variable
 echo -e "$RCLONE_CONFIG" > /root/.config/rclone/rclone.conf
 
