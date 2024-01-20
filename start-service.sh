ca816dfa6d7082e4faab0b200e4b5aa2fd28a5a9
khalidekhbo <112927910+khalidekhbo@users.noreply.github.com>
2024-01-21 01:13:09 +0600

    Update start-service.sh

--- start-service.sh
+++ start-service.sh
@@ -13,14 +13,8 @@ CONF_CONTENT=$(curl -sL "$CONF_URL")
 # Create rclone.conf
 echo "$CONF_CONTENT" > /app/rclone.conf
 
-# List remotes
-REMOTES=$(rclone --config=/app/rclone.conf listremotes)
-
-# Generate a simple HTML page with the list of remotes
-echo "<html><body><h1>Rclone Remotes</h1><pre>$REMOTES</pre></body></html>" > /app/index.html
-
-# Start a background process to serve remotes
-(rclone --config=/app/rclone.conf serve http --addr :8080 /app) &
+# Run rclone in the background
+rclone --config=/app/rclone.conf serve http --addr :8080 /app &
 
 # Start a simple web server to serve the HTML page
 python -m http.server 8081
